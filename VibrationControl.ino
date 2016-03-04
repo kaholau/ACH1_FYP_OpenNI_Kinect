ea95f0a7b4909ee76112c84ce61d164a4175bb4e
Stealownz <stealownz@gmail.com>
2016-03-04 16:11:00 +0800

    Fixed Arduino Vibration Control code to use BJTs and PIN 8,9,10

--- VibrationControl.ino
+++ VibrationControl.ino
@@ -1,32 +1,23 @@
-int leftPin1 =    9;
-int leftPin2 =    9;
+int leftPin1 =    8;
 int middlePin1 =  9;
-int middlePin2 =  9;
-int rightPin1 =   9;
-int rightPin2 =   9;
+int rightPin1 =   10;
 
 void vibrateGroup(int group) {
-  digitalWrite(leftPin1, LOW);
-  digitalWrite(leftPin2, LOW);
-  digitalWrite(middlePin1, LOW);
-  digitalWrite(middlePin2, LOW);
-  digitalWrite(rightPin1, LOW);
-  digitalWrite(rightPin2, LOW);
+  digitalWrite(leftPin1, HIGH);
+  digitalWrite(middlePin1, HIGH);
+  digitalWrite(rightPin1, HIGH);
 
   switch (group) {
     case 1: // left group
-    digitalWrite(leftPin1, HIGH);
-    digitalWrite(leftPin2, HIGH);
+    digitalWrite(leftPin1, LOW);
     break;
     
     case 2: // middle group
-    digitalWrite(middlePin1, HIGH);
-    digitalWrite(middlePin2, HIGH);
+    digitalWrite(middlePin1, LOW);
     break;
     
     case 3: // right group
-    digitalWrite(rightPin1, HIGH);
-    digitalWrite(rightPin2, HIGH);
+    digitalWrite(rightPin1, LOW);
     break;
   }
 }
@@ -37,11 +28,8 @@ void setup() {
 
   // set pins to output
   pinMode(leftPin1, OUTPUT);
-  pinMode(leftPin2, OUTPUT);
   pinMode(middlePin1, OUTPUT);
-  pinMode(middlePin2, OUTPUT);
   pinMode(rightPin1, OUTPUT);
-  pinMode(rightPin2, OUTPUT);
 }
 
 void loop() {
@@ -52,4 +40,3 @@ void loop() {
 
   vibrateGroup(group);
 }
-
